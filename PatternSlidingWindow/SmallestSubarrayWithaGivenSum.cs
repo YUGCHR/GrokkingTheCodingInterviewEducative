df81c068620150987c388076b9e822a3f2005150
Yuri Gonchar <yuri.gonchar@hotmail.com>
2021-11-23 20:36:32 +0200

    FindSmallestSubarray was added

---
+++ PatternSlidingWindow/SmallestSubarrayWithaGivenSum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSlidingWindow
+{
+    public class SmallestSubarrayWithaGivenSum
+    {
+
+        public int FindSmallestSubarray()
+        {
+            return 0;
+        }
+
+    }
+}
